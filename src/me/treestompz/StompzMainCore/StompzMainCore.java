613149e532d68b3234013e2333b18a01817598fc
Stefan Gorski <sforstefan@hotmail.com>
2013-08-08 17:09:14 -0400

    so sloppeh D:

--- src/me/treestompz/StompzMainCore/StompzMainCore.java
+++ src/me/treestompz/StompzMainCore/StompzMainCore.java
@@ -23,11 +23,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class StompzMainCore extends JavaPlugin 
 {
 
-
 	public void onEnable()
 	{		
-		//Register Listener
-		Bukkit.getPluginManager().registerEvents(new StompzMainCoreListener() , this);
+		// Register Listener
+		Bukkit.getPluginManager().registerEvents(new StompzMainCoreListener(this) , this);
 	}
 
 	public void onDisable()
@@ -35,62 +34,61 @@ public class StompzMainCore extends JavaPlugin
 
 	}
 
-	//ArrayLists used to store player's usernames
+	// ArrayList used to store player's usernames
 	ArrayList<String> playersMeow = new ArrayList<String>();
 	ArrayList<String> meowDisabledList = new ArrayList<String>();
-
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
-	{
+	{	
 		if(cmd.getName().equalsIgnoreCase("meow"))
 		{	
-			//Check if sender is a player
+			// Check if sender is a player
 			if(sender instanceof Player)
 			{
-				//Cast sender as a Player
+				// Cast sender as a Player
 				final Player player = (Player) sender;
-				//If player has the permission node "stompz.meow"
+				// If player has the permission node "stompz.meow"
 				if(player.hasPermission("stompz.meow"))
 				{
-					//If the command has not arguments
+					// If the command has not arguments
 					if(args.length == 0)
 					{
 						player.sendMessage(ChatColor.GREEN + "StompzMainCore 0.2: " + ChatColor.AQUA + "/meow" + ChatColor.GREEN + " Plugin author: " + ChatColor.AQUA + "treestompz");
 						player.sendMessage(ChatColor.RED + "Please use: /meow <playername>");
 						player.sendMessage(ChatColor.RED + "Or, to toggle meowing: /meowtoggle");
 					}
-
-					//Targetting a meow victim :D
-					//Remember, "player" is the person running the command, "target" 
+					// 2 if 4 u
+					// Targetting a meow victim :D
+					// Remember, "player" is the person running the command, "target" 
 					if(args.length == 1)						
 					{		
-						//If the player running the command is not on cooldown
+						// If the player running the command is not on cooldown
 						if(!playersMeow.contains(player.getName()))
 						{												
-							//If the player running the command has meowing disabled. Not to be confused with the target player!
+							// If the player running the command has meowing disabled. Not to be confused with the target player!
 							if(!meowDisabledList.contains(player.getName()))
 							{
-								//The target is the first argument
+								// The target is the first argument
 								Player target = getServer().getPlayer(args[0]);									
-								//If the target is a valid online player
+								// If the target is a valid online player
 								if(target != null)
 								{	
-									//If the target player has meowing disabled
+									// If the target player has meowing disabled
 									if(meowDisabledList.contains(target.getName()))
 									{
 										player.sendMessage(ChatColor.RED + "That player has meowing disabled :(");	
 										return true;
 									}
-									//If the target player does not have meowing disabled
+									// If the target player does not have meowing disabled
 									else
 									{
-										//Add the player
+										// Add the player
 										playersMeow.add(player.getName());
-										//Send a message to the player that the meow was successful
+										// Send a message to the player that the meow was successful
 										player.sendMessage(ChatColor.LIGHT_PURPLE + "You meow'd " + target.getName() + "!");
-										//Send a message to the target that he has been meowed by player AND play the meow sound
+										// Send a message to the target that he has been meowed by player AND play the meow sound
 										target.sendMessage(ChatColor.LIGHT_PURPLE + "You have been meow'd by " + sender.getName() + "! /meowtoggle to disable meowing.");	
 										target.playSound(target.getLocation(), Sound.CAT_MEOW, 10, 1);
-										//Start the /meow cooldown
+										// Start the /meow cooldown
 										Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable()
 										{
 											public void run()
@@ -99,23 +97,23 @@ public class StompzMainCore extends JavaPlugin
 												player.sendMessage(ChatColor.GREEN + "Meowdown complete.");
 												player.playSound(player.getLocation(), Sound.NOTE_PLING, 10, 1);
 											}				
-											//60 second cooldown
+											// 60 second cooldown
 										}, 20 * 60);
 									}
 								}
-								//If the target is NOT a valid online player
+								// If the target is NOT a valid online player
 								else
 								{
 									player.sendMessage(ChatColor.RED + "That player is not online or is not a valid username!");	
 								}
 							}
-							//If the player has meowing disabed
+							// If the player has meowing disabled
 							else
 							{
 								player.sendMessage(ChatColor.RED + "To /meow again, use /meowtoggle");	
 							}
 						}
-						//If the player has tried to meow again when the cooldown is still running
+						// If the player has tried to meow again when the cooldown is still running
 						else
 						{
 							player.sendMessage(ChatColor.RED + "Your /meow is still meowingdown! Meow wisely, young kitteh!");	
@@ -125,17 +123,16 @@ public class StompzMainCore extends JavaPlugin
 				}
 			}			
 		} 
-		//If the player types /meowtoggle
+		// If the player types /meowtoggle
 		if(cmd.getName().equalsIgnoreCase("meowtoggle"))
 		{
-			//If the command sender is a Player
+			// If the command sender is a Player
 			if(sender instanceof Player)
 			{
-				//If the sender has the permission "stompz.meow"
+				// If the sender has the permission "stompz.meow"
 				if(sender.hasPermission("stompz.meow"))
 				{
-					//Toggling of being able to be meow
-					
+					// Toggling of being able to be meow
 					if(!meowDisabledList.contains(sender.getName()))
 					{
 						sender.sendMessage(ChatColor.RED + "People can no longer /meow you :( Use /meowtoggle to enable!");
--- src/me/treestompz/StompzMainCore/StompzMainCoreListener.java
+++ src/me/treestompz/StompzMainCore/StompzMainCoreListener.java
@@ -2,71 +2,75 @@ package me.treestompz.StompzMainCore;
 
 
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.logging.Level;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.TNTPrimed;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBucketEmptyEvent;
-import org.bukkit.event.player.PlayerFishEvent;
-import org.bukkit.event.player.PlayerFishEvent.State;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.kitteh.tag.PlayerReceiveNameTagEvent;
 
 
 
 public class StompzMainCoreListener implements Listener
 {
 
-	public static StompzMainCoreListener instance;
+	private StompzMainCore plugin;
+	private ArrayList<String> commandCooldown;
 
-	public StompzMainCoreListener()
+	public StompzMainCoreListener(StompzMainCore plugin)
 	{
-		instance = this;        
+		this.plugin = plugin;     
+		commandCooldown = new ArrayList<String>();
 	}
 
-	
+	// When a player uses a bucket
 	@EventHandler (priority = EventPriority.MONITOR)
-	//When a player uses a bucket
 	public void blockplaceevent(PlayerBucketEmptyEvent event)
 	{
-		//If the bucket is a lava bucket AND the player does not have the permission "stompz.placelava"
+		// If the bucket is a lava bucket AND the player does not have the permission "stompz.placelava"
 		if(event.getBucket() == Material.LAVA_BUCKET && !event.getPlayer().hasPermission("stompz.placelava"))
 		{
-			//Cancel the event of dropping lava
+			// Cancel the event of dropping lava
 			event.setCancelled(true);
-			//Alert the player
+			// Alert the player
 			event.getPlayer().sendMessage(ChatColor.RED + "Non-donators cannot place lava because many new players use it to grief and kill other players! It's the sad Minecraft world we live in :(" + ChatColor.GREEN + " If you'd like to consider donating for tons of other perks, check out: " + ChatColor.AQUA + "http://stompzcraft.com/shop");
 		}
 	}
-	/*Messing with fishing-not implemented
-	@EventHandler
-	public void onFish(PlayerFishEvent event)
+
+	@EventHandler (priority = EventPriority.MONITOR)
+	public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event)
 	{
-		if(event.getState() == State.FAILED_ATTEMPT)
+		// Avoid out of bounds exception
+		if(event.getMessage().length() > 6)
 		{
-			event.getPlayer().sendMessage(ChatColor.RED + "That feels a littly heavy...");			
-			List<Entity> nearby = event.getPlayer().getNearbyEntities(50,50,50); // searches in a 100*100*100 radius around the player for other entities 
-			Entity hook = null; // holds the future hook
-			for (Entity e : nearby)
-			{ // loop through entities
-			    if (e.getType() == EntityType.FISHING_HOOK)
-			    { // it is a hook!
-			        hook = e;
-			        break;
-			    }
-			}
-			if (hook != null)
+			final Player p = event.getPlayer();
+			if(event.getMessage().substring(0, 7).equalsIgnoreCase("/ps add"))
 			{
-			    Location hookLocation = hook.getLocation(); // the location of the hook
-			    event.getPlayer().getWorld().spawnEntity(hookLocation, EntityType.SQUID);
-			} 
-			
+				if(!commandCooldown.contains(p.getName()))
+				{
+					commandCooldown.add(event.getPlayer().getName());
+					plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable()
+					{
+						public void run()
+						{
+							commandCooldown.remove(p.getName());
+						}
+					}
+					, 20 * 20);
+				}
+				else
+				{
+					event.setCancelled(true);
+					p.sendMessage(ChatColor.RED + "You are sending that command too often!");
+				}
+			}
 		}
-	}*/
+	}
 }
