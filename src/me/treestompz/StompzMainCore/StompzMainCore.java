da3be9b5068b538e51fd6a5dc8c6cf7ebaf07e90
Stefan Gorski <sforstefan@hotmail.com>
2013-08-09 19:39:49 -0400

    the BEST /reboot command EVER
    THE BEST!!!!!!111

--- src/me/treestompz/StompzMainCore/StompzMainCore.java
+++ src/me/treestompz/StompzMainCore/StompzMainCore.java
@@ -23,14 +23,20 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class StompzMainCore extends JavaPlugin 
 {
 
-	public void onEnable()
-	{		
+	// ArrayList used to store player's usernames
+	private ArrayList<String> playersMeow;
+	private ArrayList<String> meowDisabledList;
+	private int countdown;
+	private String kickMessage;
+
+	public void onEnable() {		
 		// Register Listener
 		Bukkit.getPluginManager().registerEvents(new StompzMainCoreListener(this) , this);
+		playersMeow = new ArrayList<String>();
+		meowDisabledList = new ArrayList<String>();
 	}
 
-	public void onDisable()
-	{
+	public void onDisable() {
 		/*
 		         ;'-. 
      `;-._        )  '---.._
@@ -41,11 +47,125 @@ public class StompzMainCore extends JavaPlugin
 		 */
 	}
 
-	// ArrayList used to store player's usernames
-	ArrayList<String> playersMeow = new ArrayList<String>();
-	ArrayList<String> meowDisabledList = new ArrayList<String>();
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
-	{	
+	private void sendServerBroadcast(String message, boolean isMinute) {
+		if(isMinute) {
+			Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + "[Server] Server rebooting in: " + ChatColor.GREEN + message);
+			Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + "[Server] Wondering what a \"reboot\" is? Type: " + ChatColor.AQUA + "/reboot");
+		} else {
+			Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + "[Server] Server rebooting in: " + ChatColor.GREEN + message);
+		}
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {	
+		if(cmd.getName().equalsIgnoreCase("reboot")) {
+			// Check if a non-OP'd player is running the command
+			Player p = (Player) sender;
+			if(sender instanceof Player) {
+				if(!sender.isOp()) {
+					p.sendMessage(ChatColor.DARK_GREEN + "[What is Rebooting?] " + ChatColor.GREEN + "A server reboot simply means the server is going down and then back up. " +
+							"This is necessary to perform server maintenance such as updating plugins, fixing issues, or whatever the case may be. " +
+							"Nothing will happen to any progress you made on the server unless in rare scenarios in which data was corrupted because of a server issue." +
+							"You can connect back immediately via the hub system, but the survival server might not be back online fast enough.");
+					return true;
+				}
+			}
+			if(args.length == 0) {
+				countdown = 180;
+				kickMessage = null;
+			}
+			else if(args.length == 1) {
+				try {
+					countdown = Integer.parseInt(args[0]);
+				}
+				catch (NumberFormatException ex) {
+					p.sendMessage(ChatColor.RED + "Argument was not a number!");
+				}
+			}
+			else if(args.length > 1) {
+				try {
+					countdown = Integer.parseInt(args[0]);
+				}
+				catch (NumberFormatException ex) {
+					p.sendMessage(ChatColor.RED + "Argument was not a number!");
+				}
+				kickMessage = "";
+				for(int i = 1; i < args.length; i++) {
+					kickMessage += " " + args[i];
+				}
+			}
+			this.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+				public void run() {
+
+					if(countdown <= 0) {
+						if(kickMessage != null) {
+							Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "kickall &cServer Reboot: " + ChatColor.translateAlternateColorCodes('&', kickMessage));
+						} else {
+							Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "kickall &cServer Reboot: " + ChatColor.RESET + "Join back in 30 seconds!");
+						}
+						Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "save-all");
+						if(countdown == -3) {
+							Bukkit.shutdown();
+							return;
+						}
+					}
+					// Divisible by 60
+					if(countdown % 60 == 0) {
+						if(countdown / 60 == 1) {
+							sendServerBroadcast("1 minute", true);
+						} else {
+							sendServerBroadcast(countdown/60 + " minutes", true);
+						}
+					}
+					// Divisible by 30
+					else if(countdown % 30 == 0 && countdown > 30) {
+						int temp = countdown - 30;
+						int mins = 0;
+						while(temp > 0) {
+							if(temp % 60 == 0) {
+								mins++;
+								temp -= 60;
+							}
+						}
+						if(mins == 1) {
+							sendServerBroadcast(mins + " minute and 30 seconds", false);
+						} else {
+							sendServerBroadcast(mins + " minutes and 30 seconds", false);
+						}
+					}
+
+					else if(countdown == 30) {
+						sendServerBroadcast(countdown + " seconds", false);
+					}
+
+					// 10 seconds 
+					else if (countdown == 10) {
+						sendServerBroadcast(countdown + " seconds", false);
+						for(Player p : Bukkit.getOnlinePlayers()) {
+							p.playSound(p.getLocation(), Sound.NOTE_PLING, 1, 10);
+						}
+					}
+					
+					// 10 seconds or less
+					else if (countdown <= 9 && countdown > 1) {
+						sendServerBroadcast(countdown + " seconds", false);
+						for(Player p : Bukkit.getOnlinePlayers()) {
+							p.playSound(p.getLocation(), Sound.NOTE_PLING, 1, countdown);
+						}
+					}
+					// 1 seconds
+					else if (countdown == 1) {
+						sendServerBroadcast(countdown + " second", false);
+						for(Player p : Bukkit.getOnlinePlayers()) {
+							p.playSound(p.getLocation(), Sound.CAT_MEOW, 1, countdown);
+						}
+					}
+
+					countdown--;
+				}
+			}, 0, 20);
+
+		}
+
 		if(cmd.getName().equalsIgnoreCase("meow"))
 		{	
 			// Check if sender is a player
